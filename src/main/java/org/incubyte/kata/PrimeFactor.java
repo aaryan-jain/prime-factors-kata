fbb5c156b9c410b4770590b9409faca78b72f45c
Aaryan <aaryan.vijayraj@gmail.com>
2022-05-04 18:59:31 +0530

    Test for composite passed but earliar test for prime failed

--- src/main/java/org/incubyte/kata/PrimeFactor.java
+++ src/main/java/org/incubyte/kata/PrimeFactor.java
@@ -1,6 +1,7 @@
 package org.incubyte.kata;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PrimeFactor {
@@ -9,7 +10,15 @@ public class PrimeFactor {
         if (integer == 1) {
             return primeNumbers;
         }
-        primeNumbers.add(integer);
+//        primeNumbers.add(integer);
+        int squareRoot = (int) Math.sqrt(integer);
+        for(int i=2; i<=squareRoot;i++) {
+            if(integer%i == 0) {
+                primeNumbers.add(i);
+                primeNumbers.add(integer/i);
+            }
+        }
+         Collections.sort(primeNumbers);
         return primeNumbers;
     }
 }
